33200a6e5e7b1b979b86abe10264bd52715075d5
Leopold Cambier <leopoldcambier@gmail.com>
2018-10-17 18:53:47 -0700

    Minor

--- tests/gen.jl
+++ tests/gen.jl
@@ -3,9 +3,12 @@ using MatrixMarket
 
 folder = "/scratch/groups/darve/lcambier/matrices/laplacians2/"
 
+@show folder 
+
 d = 2
 for n = [512, 1024, 2048, 4096, 8192]
     for rho = [1, 10, 100, 1000, 10000]
+        @show n, rho
         (A, a) = Ad_hc(n, d, rho)
         mmwrite("$(folder)/neglapl_$(d)_$(n)_$(rho).mm", A)
     end
