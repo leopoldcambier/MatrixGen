dca3b4e1fbd9d036701106daad6ca9852ebccfd1
Leopold Cambier <leopoldcambier@gmail.com>
2019-01-22 10:10:35 -0800

    Gen for advdiff

--- examples/gen3.jl
+++ examples/gen3.jl
@@ -2,15 +2,11 @@ include("../src/MatrixGen.jl")
 using MatrixMarket
 using Printf
 
-for (d, n) in [ (2, 256), (2, 512), (2, 1024), (3, 16), (3, 32), (3, 64), (3, 96), (3, 128) ]
-    @show d, n
-    h = 1/(n+1)
-    a = high_contrast_field(n+2, d, 10, 5)
-    function af(x)
-        i = Int.(round.((n+1) .* x)).+1
-        return a[i...]
+for b = [10, 10000]
+    for (d, n) in [ (2, 256), (2, 512), (2, 1024), (2, 2048), (2, 4096)]
+        @show d, n
+        h = 1/(n+1)
+        A, _ = elliptic_dirichlet(n, d, x -> 1, x -> [b for i= 1:d], x -> 0)
+        mmwrite(@sprintf("advdiff_%d_%d_1_%d.mm", d, n, b), A)
     end
-    A, _ = elliptic_dirichlet(n, d, af, x -> [10 for i= 1:d], x -> 0)
-    mmwrite(@sprintf("advdiff_%d_%d.mm", d, n), A)
 end
-
