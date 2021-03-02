a32d6a6a3e7eb68016c06ab1ec592ca18dda2858
Bazyli Klockiewicz <bazyli@DN51ssq0.SUNet>
2021-03-02 12:01:46 -0800

    rounded numbers

--- examples/gen.jl
+++ examples/gen.jl
@@ -5,7 +5,7 @@ folder = "mats"
 @show folder 
 
 d = 3
-for n = [50,63,80,101,127,160,202,254]
+for n = [50,60,80,100,125,160,200,250]
     for rho = [1,100]
         @show n, rho
         (A, a) = Ad_hc(n, d, rho)
--- examples/gencoord.py
+++ examples/gencoord.py
@@ -4,7 +4,7 @@ from scipy.io import mmwrite
 d = 3
 h = [0.1,0.1,0.1] # exact values are not relevant but pick the same here
 folder = 'mats/'
-for n in [50,63,80,101,127,160,202,254]:
+for n in [50,60,80,100,125,160,200,250]:
 	gridm = n
 	gridn = n
 	coordinates = np.zeros((d,n**d))
@@ -14,3 +14,4 @@ for n in [50,63,80,101,127,160,202,254]:
 	    coordinates[1,i] = iy
 	    coordinates[2,i] = iz
 	mmwrite(folder+'neglapl_coord3d_{}_{}_{}'.format(n,n,n),coordinates)
+	print('written to ' + 'neglapl_coord3d_{}_{}_{}'.format(n,n,n),coordinates)
