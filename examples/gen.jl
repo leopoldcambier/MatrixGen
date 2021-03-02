3c1da9b1f76e3a3b9007617901898be4e93556eb
Bazyli Klockiewicz <bazyli@DN51ssq0.SUNet>
2021-03-02 10:00:28 -0800

    gen script

--- examples/gen.jl
+++ examples/gen.jl
@@ -5,8 +5,8 @@ folder = "mats"
 @show folder 
 
 d = 3
-for n = [10,20,30,40,50,60]
-    for rho = [1, 10, 100, 1000]
+for n = [50,63,80,101,127,160,202,254]
+    for rho = [1,100]
         @show n, rho
         (A, a) = Ad_hc(n, d, rho)
         mmwrite("$(folder)/neglapl_$(d)_$(n)_$(rho).mm", A)
