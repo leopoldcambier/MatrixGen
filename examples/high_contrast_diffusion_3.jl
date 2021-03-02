813947997db48b53e7449f2a4d259874e9bf4bae
Bazyli Klockiewicz <bazyli@DN51ssq0.SUNet>
2021-03-02 09:35:48 -0800

    exact numbers for testing

--- examples/high_contrast_diffusion_3.jl
+++ examples/high_contrast_diffusion_3.jl
@@ -3,8 +3,8 @@ using Plots
 gr()
 
 ## Generate high contrast laplacians
-for n = [10]
-    for rho = [100]
+for n = [50,63,80,101,127,160,202,254]
+    for rho = [1,100]
         println("Rho = ", rho, " n = ", n)
         (A, a) = Ad_hc(n, 3, rho)
         println("Cond(A) +-= ", estimate_cond2(A))
