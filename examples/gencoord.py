cc76afe8c38a1813bfed61a66d57ae7acebd9b31
Bazyli Jan Klockiewicz <bazyli@sh03-ln03.stanford.edu>
2021-03-02 21:01:14 -0800

    fix bug in gencoord

--- examples/gencoord.py
+++ examples/gencoord.py
@@ -1,15 +1,15 @@
 import numpy as np
 from scipy.io import mmwrite
 
-d = 3
+d = 2
 assert(d == 2 or d == 3)
 h = [0.1,0.1,0.1] # exact values are not relevant but pick the same here
 folder = 'mats/'
-for n in [100,200,400,800,1600,3200,6400]:
+for n in [100,200,400,800,1600,3200,6400,12800,25600]:
 	gridm = n
 	gridn = n
 	coordinates = np.zeros((d,n**d))
-	for i in range(n**3):
+	for i in range(n**d):
 	    ix = h[0] * (i % gridm); iy = h[1] * ((i % (gridm * gridn)) // gridm); iz = h[2] * (i // (gridm * gridn))
 	    coordinates[0,i] = ix
 	    coordinates[1,i] = iy
@@ -20,4 +20,4 @@ for n in [100,200,400,800,1600,3200,6400]:
 		print('written to ' + 'neglapl_coord3d_{}_{}_{}'.format(n,n,n))
 	else:
 		mmwrite(folder+'neglapl_coord2d_{}_{}'.format(n,n),coordinates)
-		print('written to ' + 'neglapl_coord3d_{}_{}'.format(n,n))		
+		print('written to ' + 'neglapl_coord2d_{}_{}'.format(n,n))		
