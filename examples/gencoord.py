c37d247ad4baec934fee94d43619ea395a17572c
Bazyli Klockiewicz <bazyli@DN51ssq0.SUNet>
2021-03-02 19:41:07 -0800

    minor

--- examples/gencoord.py
+++ examples/gencoord.py
@@ -2,9 +2,10 @@ import numpy as np
 from scipy.io import mmwrite
 
 d = 3
+assert(d == 2 or d == 3)
 h = [0.1,0.1,0.1] # exact values are not relevant but pick the same here
 folder = 'mats/'
-for n in [50,60,80,100,125,160,200,250]:
+for n in [100,200,400,800,1600,3200,6400]:
 	gridm = n
 	gridn = n
 	coordinates = np.zeros((d,n**d))
@@ -12,6 +13,11 @@ for n in [50,60,80,100,125,160,200,250]:
 	    ix = h[0] * (i % gridm); iy = h[1] * ((i % (gridm * gridn)) // gridm); iz = h[2] * (i // (gridm * gridn))
 	    coordinates[0,i] = ix
 	    coordinates[1,i] = iy
-	    coordinates[2,i] = iz
-	mmwrite(folder+'neglapl_coord3d_{}_{}_{}'.format(n,n,n),coordinates)
-	print('written to ' + 'neglapl_coord3d_{}_{}_{}'.format(n,n,n))
+	    if d == 3:
+	    	coordinates[2,i] = iz
+	if d == 3:
+		mmwrite(folder+'neglapl_coord3d_{}_{}_{}'.format(n,n,n),coordinates)
+		print('written to ' + 'neglapl_coord3d_{}_{}_{}'.format(n,n,n))
+	else:
+		mmwrite(folder+'neglapl_coord2d_{}_{}'.format(n,n),coordinates)
+		print('written to ' + 'neglapl_coord3d_{}_{}'.format(n,n))		
